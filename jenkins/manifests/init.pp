493eea44868abfcb2b512f16b2caa30437d0b890
Cosimo Streppone <cosimo@opera.com>
2011-03-16 23:51:56 +1100

    Added module to install a complete Jenkins instance

---
+++ jenkins/manifests/init.pp
@@ -0,0 +1,46 @@
+class jenkins {
+
+    include base_packages
+    include apt
+    include java
+    include apache
+
+    apt::key { "D50582E6":
+        source => "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key",
+    }
+
+    apt::sources_list { "jenkins-ci":
+        ensure  => "present",
+        content => "deb http://pkg.jenkins-ci.org/debian binary/",
+        require => Apt::Key["D50582E6"],
+        notify => Exec["apt-get_update"]
+    }
+
+    package { "jenkins":
+        ensure => "installed",
+	require => Apt::Sources_list["jenkins-ci"],
+    }
+
+    # Setup apache to forward port 80 to 8080
+    apache::module { "proxy": }
+    apache::module { "proxy_http": }
+    apache::module { "vhost_alias": }
+
+    apache::vhost { "default":
+        ensure => "absent",
+    }
+
+    apache::vhost { "jenkins":
+        ensure => "present",
+        source => "puppet:///modules/jenkins/apache-vhost",
+    }
+
+    file { "/etc/sudoers.d/jenkins-nopasswd":
+        ensure => "present",
+        source => "puppet:///modules/jenkins/sudo-rule",
+        # base_packages also contains 'sudo'
+        require => Class["base_packages"],
+    }
+
+}
+
