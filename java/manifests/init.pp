6f693ace8c03f3d66578794191463af2ff8a2b32
Cosimo Streppone <cosimo@opera.com>
2011-12-13 08:24:35 +0100

    Now with automatic license acceptance

--- java/manifests/init.pp
+++ java/manifests/init.pp
@@ -1,12 +1,38 @@
+class sun_java6 {
+
+    exec {
+        #"/bin/echo \"sun-java6-plugin shared/accepted-sun-dlj-v1-1 boolean true\" | /usr/bin/debconf-set-selections":
+        #alias => "debconf-set-selections-sun-java6-plugin";
+
+        "/bin/echo \"sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true\" | /usr/bin/debconf-set-selections":
+        alias => "debconf-set-selections-sun-java6-bin";
+
+        "/bin/echo \"sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true\" | /usr/bin/debconf-set-selections":
+        alias => "debconf-set-selections-sun-java6-jre";
+    } 
+
+    package { 
+        #"sun-java6-plugin":
+        #    require => [
+        #        Exec["debconf-set-selections-sun-java6-plugin"],
+        #        Package["sun-java6-bin"],
+        #        Package["sun-java6-jre"] ],
+        #    ensure => "installed";
+
+        "sun-java6-bin":
+            require => Exec["debconf-set-selections-sun-java6-bin"],
+            ensure => "installed";
+
+        "sun-java6-jre":
+            require => Exec["debconf-set-selections-sun-java6-jre"],
+            ensure => "installed";
+    }
+
+}
+
 class java {
 
-    package { "sun-java6-bin":
-        ensure => "installed"
-    }
-
-    package { "sun-java6-jre":
-        ensure => "installed"
-    }
+    include sun_java6
 
 }
 
