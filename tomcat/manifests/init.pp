444d844643faad136abe13c96524bfe2bdb02487
Cosimo Streppone <cosimo@opera.com>
2011-12-13 08:29:32 +0100

    Logging tweak to avoid catalina.out filling disks

--- tomcat/manifests/init.pp
+++ tomcat/manifests/init.pp
@@ -27,6 +27,16 @@ class tomcat {
         require => Package["tomcat6"],
     }
 
+    # Just to disable console logging that fills up catalina.out
+    # log file with useless junk
+    file { "/etc/tomcat6/logging.properties":
+        ensure => "present",
+        source => "puppet:///modules/tomcat/logging.properties",
+        owner  => "root",
+        group  => "root",
+        require => Package["tomcat6"],
+    }
+
     munin::plugin {
         [ "tomcat_jvm", "tomcat_threads", "tomcat_volume", "tomcat_access" ]:
         ensure => "present",
@@ -35,7 +45,11 @@ class tomcat {
 
     service { "tomcat6":
        ensure => "running",
-       require => [ Package["tomcat6"], File["/etc/tomcat6/tomcat-users.xml"] ],
+       require => [
+          Package["tomcat6"],
+          File["/etc/tomcat6/tomcat-users.xml"],
+          File["/etc/tomcat6/logging.properties"]
+       ],
        subscribe => Package["tomcat6"],
     }
 
